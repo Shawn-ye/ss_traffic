304be5cdc432f4b2321b0415f0ff52e429d4b843
shawn <shuang.ye@ecovacs.com>
2017-11-13 00:45:56 -0500

    jsldjflsd

--- tools/remote.sh
+++ tools/remote.sh
@@ -5,7 +5,8 @@ for id in `docker ps -qa --filter=status=running`
 # docker ps | while read line
 do
     PORT_CONFIG=`docker port $id`
-    echo "CONTAINER ID : $id, PORT CONFIG: $PORT_CONFIG"
+    TRAFFIC_LIMIT=`docker exec $id /bin/bash -c 'echo "\$TRAFFIC_LIMIT"'`
+    echo "CONTAINER ID : $id, PORT CONFIG: $PORT_CONFIG, TRAFFIC LIMIT : $TRAFFIC_LIMIT"
     cat ss_env/$id*.log | grep "TRAFFIC-COUNT-TX"
     echo
 done
